5fa2ce31d6f420e54fe1227febcfe3a8465dd5dc
Graham Klyne <gk-github@ninebynine.org>
2018-06-28 17:49:44 +0100

    Add initial skelton for GeoNames core data extractor.

---
+++ src/coredataextractor/README.md
@@ -0,0 +1,4 @@
+# Core data extractor
+
+This directory contains code for a commasnd line tool to extract EMPlaces core data, and other useful values, based on a GeoNames ID.
+
---
+++ src/coredataextractor/get_core_data.py
@@ -0,0 +1,129 @@
+# !/usr/bin/env python
+#
+# am_main.py - command line tool to create EMPlaces core from GeoNames data
+#
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__author__      = "Graham Klyne (GK@ACM.ORG)"
+__copyright__   = "Copyright 2018, Graham Klyne and University of Oxford"
+__license__     = "MIT (http://opensource.org/licenses/MIT)"
+
+import sys
+import os
+import os.path
+import re
+import argparse
+import logging
+import errno
+
+log = logging.getLogger(__name__)
+
+# if __name__ == "__main__":
+dirhere = os.path.dirname(os.path.realpath(__file__))
+annroot = os.path.dirname(os.path.join(dirhere))
+sys.path.insert(0, annroot)
+# sys.path.insert(0, dirhere)
+
+VERSION = "0.1"
+
+# Status return codes
+
+GCD_SUCCESS          = 0         # Success
+GCD_BADCMD           = 2         # Command error
+GCD_UNEXPECTEDARGS   = 3         # Unexpected arguments supplied
+
+def progname(args):
+    return os.path.basename(args[0])
+
+def parseCommandArgs(argv):
+    """
+    Parse command line arguments
+
+    argv            argument list from command line
+
+    Returns a pair consisting of options specified as returned by
+    OptionParser, and any remaining unparsed arguments.
+    """
+    # create a parser for the command line options
+    parser = argparse.ArgumentParser(
+                description="EMPlaces GeoNames data extractor",
+                formatter_class=argparse.RawDescriptionHelpFormatter,
+                epilog=command_summary_help
+                )
+    parser.add_argument('--version', action='version', version='%(prog)s '+VERSION)
+    parser.add_argument("--debug",
+                        action="store_true", 
+                        dest="debug", 
+                        default=False,
+                        help="Run with full debug output enabled.  "+
+                             "Also creates log file 'annalist-manager.log' in the working directory"
+                        )
+    parser.add_argument("command", metavar="COMMAND",
+                        nargs=None,
+                        help="sub-command, one of the options listed below."
+                        )
+    parser.add_argument("args", metavar="ARGS",
+                        nargs="*",
+                        help="Additional arguments, depending on the command used."
+                        )
+    # parse command line now
+    options = parser.parse_args(argv)
+    if options:
+        if options and options.command:
+            return options
+    print("No valid usage option given.", file=sys.stderr)
+    parser.print_usage()
+    return None
+
+def run(userhome, userconfig, options, progname):
+    """
+    Command line tool to create and submit deposit information packages
+    """
+    if options.command.startswith("@@@"):
+        return do_zzzzzz(annroot, options)
+    if options.command.startswith("ver"):                   # version
+        return show_version(annroot, userhome, options)
+    if options.command.startswith("help"):
+        return show_help(options, progname)
+    print("Un-recognised sub-command: %s"%(options.command), file=sys.stderr)
+    print("Use '%s --help' to see usage summary"%(progname), file=sys.stderr)        
+    return am_errors.AM_BADCMD
+
+def runCommand(userhome, userconfig, argv):
+    """
+    Run program with supplied configuration base directory, Base directory
+    from which to start looking for research objects, and arguments.
+
+    This is called by main function (below), and also by test suite routines.
+
+    Returns exit status.
+    """
+    options = parseCommandArgs(argv[1:])
+    if options:
+        progname = os.path.basename(argv[0])
+        status   = run(userhome, userconfig, options, progname)
+    else:
+        status = am_errors.GCD_BADCMD
+    return status
+
+def runMain():
+    """
+    Main program transfer function for setup.py console script
+    """
+    userhome   = os.path.expanduser("~")
+    userconfig = os.path.join(userhome, ".emplaces")
+    return runCommand(userhome, userconfig, sys.argv)
+
+if __name__ == "__main__":
+    """
+    Program invoked from the command line.
+    """
+    p = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    sys.path.insert(0, p)
+    status = runMain()
+    sys.exit(status)
+
+# End.
+
